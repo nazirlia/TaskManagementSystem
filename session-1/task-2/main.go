cea9a2d4266937844bc9b30f1c78b50fde1674f8
Aykhan Nazirli <nazirlia@yahoo.com>
2026-01-23 12:34:07 +0400

    placed session-1's tasks

---
+++ session-1/task-2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	message := `Welcome to the Task Management System!
+Project start date is: 2026-01-20 00:00:00
+Project: Task Management System`
+
+	fmt.Println(message)
+}
